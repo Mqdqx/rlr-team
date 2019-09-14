8d5bb1ae081a04a033329725b44ff43d4b010ea8
chitanda <eru@yui.com>
2019-09-14 22:49:31 +0800

    render A/index

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -59,7 +59,7 @@ class AdminController extends Controller
      */
     public function actionIndex()
     {
-        //return $this->render('index');
+        return $this->render('index');
     }
     
 }
