e11d688af7081d7d5fa0aefb33fff96eeb2aee42
chitanda <liu972436798@163.com>
2019-09-15 00:37:07 +0800

    modify C/admin/index

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -59,7 +59,7 @@ class AdminController extends Controller
      */
     public function actionIndex()
     {
-        return $this->render('site/index');
+        return $this->render('index');
     }
     
 }
